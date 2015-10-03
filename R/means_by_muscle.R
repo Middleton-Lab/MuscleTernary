ea49c488170b98f52b4ca3992bb6e32987f807b0
Kevin Middleton <middletonk@missouri.edu>
2015-10-02 22:07:20 -0500

    Handle an arbitrary number of catogorical variables in means_by_muscle().

--- R/means_by_muscle.R
+++ R/means_by_muscle.R
@@ -10,15 +10,36 @@
 #'
 means_by_muscle <- function(df_no_means){
 
-  Muscle_Group <- df_no_means$Muscle_Group
+  # data.frame to hold factors
+  categorical_vars <- data.frame(muscle = df_no_means$muscle)
 
-  message("Assuming that any categorical variables alternate by rows.")
+  # variables to drop
+  drop_vars <- list()
+
+  # Find categorical variables
+  for (i in 2:ncol(df_no_means)){
+    variable_name <- names(df_no_means)[i]
+    tmp <- df_no_means[, i]
+    if (is.factor(tmp)){
+      categorical_vars[, variable_name] <- tmp
+      drop_vars <- c(drop_vars, variable_name)
+    }
+  }
+
+  # Drop non-numeric variables
+  df_no_means <-
+    df_no_means[ , !(names(df_no_means) %in% unlist(drop_vars))]
+
+  message("\nAssuming that any categorical variables alternate by rows.\n")
 
   # Means by Left_Right
   df_means <- df_no_means %>% group_by(muscle) %>% summarise_each(funs(mean))
 
-  # Put Muscle_Group back on. Note: Assumes alternating rows
-  df_means$Muscle_Group <- Muscle_Group[seq(1, nrow(df_no_means), by = 2)]
+  # Put categorical variables back on. Note: Assumes alternating rows
+  categorical_vars <-
+    categorical_vars[seq(1, nrow(categorical_vars), by = 2), ]
+
+  df_means <- merge(df_means, categorical_vars)
 
   return(df_means)
 }
