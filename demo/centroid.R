31499bd3c5062ca8f5cd203dc6e110df331fb843
Kevin Middleton <middletonk@missouri.edu>
2022-12-08 18:04:34 -0600

    Add centroid demo

---
+++ demo/centroid.R
@@ -0,0 +1,9 @@
+library(MuscleTernary)
+
+centroid_location(system.file("extdata",
+                              "L_mAMEM_or.stl",
+                              package = "MuscleTernary"))
+
+centroid_size(system.file("extdata",
+                          "L_mAMEM_or.stl",
+                          package = "MuscleTernary"))
